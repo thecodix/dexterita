43232c87ae534f35e8b9068d0c2f4a995f5266b5
Alfredo Romeu <romeu.alfred@gmail.com>
2020-07-04 14:26:21 +0200

    Configuration for metadata segundo

--- src/json/_meta/segundo/index.js
+++ src/json/_meta/segundo/index.js
@@ -1,7 +1,15 @@
+import DESARROLLOI from './desarrollo-i.json';
+import DIFERENCIAS_INDIVIDUALES from './diferencias-individuales.json';
+import DISENOS_INVESTIGACION from './disenos-investigacion.json';
 import FISIOLOGICA from './psicologia-fisiologica.json';
+import MEMORIA from './memoria.json';
+import PENSAMIENTO from './pensamiento.json';
 import PERCEPCION from './percepcion.json';
+import PSICOMETRIA from './psicometria.json';
+import PSICOPATOLOGIA from './psicopatologia.json';
 
 // eslint-disable-next-line import/prefer-default-export
-export const ASIGNATURAS_SEGUNDO = Object.freeze(FISIOLOGICA.concat(
-  PERCEPCION,
+export const ASIGNATURAS_SEGUNDO = Object.freeze(DESARROLLOI.concat(
+  DIFERENCIAS_INDIVIDUALES, DISENOS_INVESTIGACION, FISIOLOGICA, MEMORIA,
+  PENSAMIENTO, PERCEPCION, PSICOMETRIA, PSICOPATOLOGIA,
 ));
